75b417c958ac06e4cd1a0cd32ca3cc98af66c25c
gabriella <gabriellabraga.academico@gmail.com>
2023-05-16 21:36:54 -0300

    Adiciona questao 03 do livro Batista e Moraes

---
+++ BMQuestao03/TesteConversao.java
@@ -0,0 +1,8 @@
+public class TesteConversao {
+    public static void main(String[] args) {
+        System.out.println(ConversaoDeUnidadesDeArea.metroParaPesQuadrados(100));
+        System.out.println(ConversaoDeUnidadesDeArea.pesParaCentimetroQuadrado(100));
+        System.out.println(ConversaoDeUnidadesDeArea.milhaParaAcres(100));
+        System.out.println(ConversaoDeUnidadesDeArea.acreParaPesQuadrados(100));
+    }
+}
